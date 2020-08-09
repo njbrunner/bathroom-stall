a5ff0d8bbccb26c05beadc60c781502a1214ec95
Nicholas Brunner <nicholas.brunner@jhuapl.edu>
2020-08-09 18:11:42 -0400

    Added new post toolbar

--- src/components/NewPost.vue
+++ src/components/NewPost.vue
@@ -1,41 +1,109 @@
 <template>
-<div class="container">
-    <div @mousedown="pointerDown" class="new-post-container" ref="newPost">
-        <p class="new-post-text">{{ text }}</p>
-        <v-btn
-        fab
-        x-small
-        class="button-info"
-        @click="toggleInfo">
-            <v-icon>mdi-information-outline</v-icon>
-        </v-btn>
+<div>
+    <div
+        class="new-post-container"
+        ref="newPost">
+        <div
+            class="new-post-toolbar"
+            v-bind:class="{'hide-toolbar': mouseDown}">
+            <button>
+                <i class="mdi mdi-format-font"></i>
+                <i class="mdi mdi-menu-down"></i>
+            </button>
+            <ul class="dropdown-menu">
+                <li
+                    class="dropdown-menu-item"
+                    v-for="(font, index) in fonts"
+                    :key="index"
+                    v-bind:style="{'font-family': font}">
+                    {{ font }}
+                </li>
+            </ul>
+            <button>
+                <i class="mdi mdi-format-size"></i>
+                <i class="mdi mdi-menu-down"></i>
+            </button>
+            <ul class="dropdown-menu">
+                <li
+                    class="dropdown-menu-item"
+                    v-for="(size, index) in fontSizes"
+                    :key="index">
+                    {{ size }}
+                </li>
+            </ul>
+            <button @click="openColorPicker">
+                <i class="mdi mdi-palette"></i>
+                <i class="mdi mdi-menu-down"></i>
+            </button>
+            <input
+                type="color"
+                class="element-display-none"
+                id="colorPicker"
+                ref="colorPicker">
+            <!-- <div id="font-picker"></div> -->
+            <!-- <v-select
+                :items=fontSizes
+                v-model="selectedFontSize"
+                solo>
+            </v-select>
+            <input
+                type="color"
+                v-model="selectedFontColor"
+                @change="test"> -->
+            <v-btn
+                outlined>
+                Post
+            </v-btn>
+        </div>
+        <div
+            @mousedown="pointerDown"
+            class="new-post-input-container">
+            <v-textarea
+                class="new-post-input apply-font"
+                id="new-post-input"
+                v-model="text"
+                rows="1"
+                auto-grow
+                outlined
+                :style="{'font-size': selectedFontSize, 'color': selectedFontColor + '!important'}">
+            </v-textarea>
 
-        <div v-if="infoBox" class="info-box">
-            <p>Mouse Down: {{ mouseDown }}</p>
-            <p>Position 1: {{ pos1 }}</p>
-            <p>Position 2: {{ pos2 }}</p>
-            <p>Position 3: {{ pos3 }}</p>
-            <p>Position 4: {{ pos4 }}</p>
+
         </div>
     </div>
 </div>
 </template>
 
 <script>
+// import axios from "axios";
+// import FontPicker from "font-picker";
+// import WebFont from "webfontloader";
+
 export default {
     name: "NewPost",
     data() {
         return {
-            text: "New Post",
+            text: null,
             pos1: 0,
             pos2: 0,
             pos3: 0,
             pos4: 0,
             mouseDown: false,
-            infoBox: false
+            fontSizes: ['small', 'medium', 'large'],
+            selectedFontSize: 'small',
+            selectedFontColor: '#000000',
+            fonts: ['Roboto', 'Pacifico'],
+        }
+    },
+    computed: {
+        fontStyle(fontName) {
+            return "font-family: " + fontName;
         }
     },
     methods: {
+        test() {
+            console.log(this.selectedFontColor);
+        },
         pointerDown: function(event) {
             event = event || window.event;
             event.preventDefault();
@@ -69,36 +137,68 @@ export default {
             element.style.top = (element.offsetTop - this.pos2) + "px";
             element.style.left = (element.offsetLeft - this.pos1) + "px";
         },
-        toggleInfo() {
-            this.infoBox = !this.infoBox
+        openColorPicker() {
+            this.$refs.colorPicker.click();
         },
     },
     mounted() {
         this.$refs.newPost.style.left = "50vw";
         this.$refs.newPost.style.top = "50vh";
-    // transform: translate(-50%, -50%);
-    }
+        // const api_key = "AIzaSyArcqIdXZv6-F2LCQ0Mam6aBJ0X7Xzbhd0";
+        // const fontPicker = new FontPicker(  // eslint-disable-line no-unused-vars
+        //     api_key, // Google API key
+        //     "Open Sans", // Default font
+        //     { limit: 250 }, // Additional options
+        // );
+        // console.log(fontPicker.getFonts());
+        // this.fonts = fontPicker.getFonts();
+        // console.log(this.fonts[0]);
+        // WebFont.load({
+        //     google: {
+        //         families: ['Roboto', 'Pacifico']
+        //     }
+        // });
+    },
 }
 </script>
 
 <style>
 .new-post-container {
     position: fixed;
-    z-index: 10;
+    z-index: 15;
+    /* border: dashed black 1px; */
 }
-.new-post-text {
-    padding: 15px;
-    border: dashed black 1px;
+.new-post-input-container {
+    padding: 8px 8px 0 8px;
     cursor: move;
+    /* border: dashed red 1px; */
 }
-.container {
-    margin-top: 100px;
+.hide-toolbar {
+    visibility: hidden;
 }
-.info-box {
-    margin-top: 16px;
+
+.element-display-none {
+    display: none;
 }
-.button-info {
-    margin-right: 0;
-    padding-right: 0;
+
+.dropdown-menu {
+  list-style: none;
+  border: 1px solid black;
+  border-radius: 5px;
+  width: 100px;
+  padding: 0;
+  margin-top: 5px;
+  max-height: 100px;
+  overflow-y: scroll;
+  /* display: none; */
+}
+
+.dropdown-menu-item {
+  padding: 5px;
+  cursor: pointer;
+}
+
+.dropdown-menu-item:hover {
+    background-color: lightgrey;
 }
 </style>
